0e671604a664b2aaf5aa779249afcfe5cb4989fb
Speshkitty <sirspeshkitty+github@gmail.com>
2021-01-01 16:34:51 +0000

    Add project files.

---
+++ StardewMods/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardewMods
+{
+    public class Class1
+    {
+    }
+}
