95a3bcb76aac595c02213fba62eb9a184a134350
Speshkitty <sirspeshkitty+github@gmail.com>
2024-04-16 12:51:36 +0100

    Updated to SDV 1.6

--- QueenOfSauceRandomiser/ModEntry.cs
+++ QueenOfSauceRandomiser/ModEntry.cs
@@ -3,10 +3,11 @@ using StardewModdingAPI.Events;
 using StardewValley;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace QueenOfSauceRandomiser
 {
-    public class ModEntry : Mod, IAssetEditor
+    public class ModEntry : Mod
     {
         //We need to save and load data about the shuffle
 
@@ -27,15 +28,40 @@ namespace QueenOfSauceRandomiser
             Helper.Events.GameLoop.SaveLoaded += GameLoop_SaveLoaded;
             Helper.Events.GameLoop.SaveCreating += GameLoop_SaveCreating;
 
+            Helper.Events.Content.AssetRequested += Content_AssetRequested;
+
             Helper.Events.Multiplayer.PeerConnected += Multiplayer.PeerConnected;
             Helper.Events.Multiplayer.ModMessageReceived += Multiplayer.ModMessageReceived;
         }
 
+        private void Content_AssetRequested(object sender, AssetRequestedEventArgs e)
+        {
+            if (!e.NameWithoutLocale.IsEquivalentTo(XNBDataPath)) { return; }
+
+            e.Edit(asset =>
+            {
+                if(ShuffleData == null || ShuffleData.Count== 0) { return; }
+                IDictionary<string, string> BaseData = asset.AsDictionary<string, string>().Data;
+
+                foreach (KeyValuePair<string, string> kvp in new Dictionary<string, string>(BaseData))
+                {
+                    if (ShuffleData.TryGetValue(int.Parse(kvp.Key), out int NewID))
+                    {
+                        BaseData[NewID.ToString()] = kvp.Value;
+                    }
+                    else
+                    {
+                        BaseData[kvp.Key] = kvp.Value;
+                    }
+                }
+            });
+        }
+
         //At this step, we want to shuffle the recipes as this is when the save is initially created
-        private void GameLoop_SaveCreating(object sender, SaveCreatingEventArgs e)
+        private void GameLoop_SaveCreating(object sender, SaveCreatingEventArgs? e)
         {
             //We need to find out how many QoS recipes there are by loading the file:
-            IDictionary<string, string> BaseQoSRecipes = Helper.Content.Load<IDictionary<string, string>>(XNBDataPath, ContentSource.GameContent);
+            IDictionary<string, string> BaseQoSRecipes = DataLoader.Tv_CookingChannel(Game1.content);
 
             //the format:
             // "IDNum" : "text data
@@ -57,7 +83,7 @@ namespace QueenOfSauceRandomiser
             Helper.Data.WriteSaveData(OurDataPath, ShuffleData);
         }
 
-        private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e)
+        private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs? e)
         {
             if (!Context.IsMainPlayer)
             {
@@ -65,25 +91,7 @@ namespace QueenOfSauceRandomiser
             }
 
             //Load the list of shuffled recipes
-            ShuffleData = Helper.Data.ReadSaveData<Dictionary<int, int>>(OurDataPath);
-        }
-
-        public bool CanEdit<T>(IAssetInfo asset) => asset.AssetNameEquals(XNBDataPath) && ShuffleData != null && ShuffleData.Count != 0;
-
-        public void Edit<T>(IAssetData asset)
-        {
-            IDictionary<string, string> BaseData = asset.AsDictionary<string, string>().Data;
-            foreach (KeyValuePair<string, string> kvp in new Dictionary<string, string>(BaseData))
-            {
-                if (ShuffleData.TryGetValue(int.Parse(kvp.Key), out int NewID))
-                {
-                    BaseData[NewID.ToString()] = kvp.Value;
-                }
-                else
-                {
-                    BaseData[kvp.Key] = kvp.Value;
-                }
-            }
+            ShuffleData = Helper.Data.ReadSaveData<Dictionary<int, int>>(OurDataPath) ?? new Dictionary<int, int>();
         }
     }
 }
