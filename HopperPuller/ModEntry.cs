aa8db461e82677e244d6bdb897a4fb75f0a17e78
Speshkitty <sirspeshkitty+github@gmail.com>
2024-04-22 14:50:48 +0100

    Fixed hopper chaining

--- HopperPuller/ModEntry.cs
+++ HopperPuller/ModEntry.cs
@@ -2,8 +2,10 @@
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.GameData.BigCraftables;
 using StardewValley.Objects;
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using SObject = StardewValley.Object;
 
 namespace HopperPuller
@@ -26,10 +28,24 @@ namespace HopperPuller
                 GameLocation position = __instance.Location;
                 Vector2 tile = __instance.TileLocation;
                 SObject? belowItem = position.getObjectAtTile((int)tile.X, (int)tile.Y + 1);
-                Chest? belowChest;
-                if (belowItem != null && belowItem is Chest && (belowChest = (belowItem as Chest))!.SpecialChestType == Chest.SpecialChestTypes.AutoLoader)
+
+                if (belowItem != null && belowItem is Chest belowChest && belowChest.SpecialChestType == Chest.SpecialChestTypes.AutoLoader) //hopper below this machine
                 {
                     __instance.heldObject.Value = (SObject)belowChest.addItem(__instance.heldObject.Value);
+                    __instance.readyForHarvest.Value = false;
+                    if (__instance is Cask cask)
+                    {
+                        cask.MinutesUntilReady = -1;
+                        cask.agingRate.Value = 0;
+                        cask.daysToMature.Value = 0;
+                    }
+                    __instance.AttemptAutoLoad(new Farmer());
+
+                    if(position.getObjectAtTile((int)belowChest.TileLocation.X, (int)belowChest.TileLocation.Y + 1) is SObject machine) //pass it down the chain
+                    {
+                        machine.AttemptAutoLoad(new Farmer());
+                    }
+                    //__instance.checkForAction(new Farmer());
                 }
             }
         }
